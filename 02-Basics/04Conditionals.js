a672ad774fc2f4182b45efe4788658952ae1c2e0
M0315G <manish15gupta03@gmail.com>
2021-06-04 21:16:56 +0530

    JS Conditional Statements

---
+++ 02-Basics/04Conditionals.js
@@ -0,0 +1,13 @@
+var temperature;
+
+// TODO: Go to Google and get the data
+
+temperature = 20;
+
+if (temperature < 20){
+    console.log("It's very cold outside");
+}else if (temperature < 40 && temperature >=20){
+    console.log("The atmosphere is good outside.");
+}else if (temperature > 40){
+    console.log("It's very hot outside");
+}
