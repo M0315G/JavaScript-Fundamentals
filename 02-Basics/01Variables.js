42384618adebe4641ab02d29f561c8adfcb069c6
M0315G <manish15gupta03@gmail.com>
2021-06-04 21:16:29 +0530

    JS Variables

---
+++ 02-Basics/01Variables.js
@@ -0,0 +1,19 @@
+// String variables
+var fullName = "Manish Gupta";
+var courseName = "React JS Bootcamp";
+
+// Boolean types {true, false}
+var isLoggedIn = true;
+
+// Integer varibales
+var loggedCount = 34;
+
+console.log(loggedCount);
+
+// initalization of variable
+var paymentMode;
+console.log(paymentMode);  // Gives output as "undefined"
+
+paymentMode = "Credit Card";
+
+
