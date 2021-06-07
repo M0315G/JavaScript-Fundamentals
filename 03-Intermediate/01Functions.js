968e5c0b07e79e022061e17584337464cd5a6f0a
M0315G <manish15gupta03@gmail.com>
2021-06-07 11:51:36 +0530

    Functions in JS

---
+++ 03-Intermediate/01Functions.js
@@ -0,0 +1,15 @@
+function sayHello(name){
+    console.log("Hello World again!!");
+    //console.log("Hello there,", name);
+    console.log(`Hello there, ${name}. How are you?`);
+}
+
+// sayHello("Manish");
+// sayHello("Hitesh");
+
+function namstey(){
+    return "Hello from India";
+}
+
+var ans = namstey();
+console.log(ans);
