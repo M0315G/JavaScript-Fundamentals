880af922cc41c1280f8a49bef579237199c374b7
M0315G <manish15gupta03@gmail.com>
2021-06-08 21:15:44 +0530

    this keyword in JS

---
+++ 03-Intermediate/05thisKeyword.js
@@ -0,0 +1,11 @@
+// Gives us the global context
+console.log(this);
+
+var game = "basketball";
+
+// function sayName(){
+//     var name = "Manish";
+//     console.log(this);
+// }
+
+// sayName();
