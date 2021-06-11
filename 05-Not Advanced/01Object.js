19af8bec4d44a28a627dcb1eca4261bfc6c58d17
M0315G <manish15gupta03@gmail.com>
2021-06-11 16:19:28 +0530

    Objects in JS

--- 05-Not Advanced/01Object.js
+++ 05-Not Advanced/01Object.js
@@ -6,9 +6,22 @@ var User = function(firstName, courseCount){
     };
 };
 
+User.prototype.getFirstName = () => {
+    console.log(`Your First Name is : ${this.firstName}`);
+};
+
 var manish = new User("Manish", 2);
 console.log(manish);
+manish.getCourseCount();
+
+if( manish.hasOwnProperty("firstName") ){
+    manish.getFirstName();
+}   
+
+
 
 var sam = new User("Sam", 3);
 console.log(sam);
+sam.getCourseCount();
+sam.getFirstName();
 
