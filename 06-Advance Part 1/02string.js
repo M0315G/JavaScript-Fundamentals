7004a2f2e73e0896cbbb27b875f89e800a8c6b54
M0315G <manish15gupta03@gmail.com>
2021-06-14 19:13:01 +0530

    strings in JS

---
+++ 06-Advance Part 1/02string.js
@@ -0,0 +1,8 @@
+var name = "manish";
+var fullName = `${name} Gupta`;
+
+console.log(fullName);
+console.log(fullName.charAt(0));
+console.log(fullName.endsWith("!"));
+console.log(fullName.includes("manish"));
+console.log(fullName.toUpperCase());
